65bd8a6eb8b865b9f351e293617b981c4d14e146
Amy <0814233040@s.tusd1.org>
2026-03-10 21:57:52 +0000

    almost.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Script started");
+
+function flipCard(cardId) {
+    console.log("click");
+
+    let card= document.getElementById(cardId);
+    card.style.color = "rgb(251, 255, 0)";
+    card.style.backgroundColor ="rgb(255, 187, 0)";
+}
+
